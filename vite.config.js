db4c0e010b01d1555c1c0d9be93bce1f314c3edc
naovich <attoumani.nadhoir@gmail.com>
2025-03-07 18:07:13 +0100

    Fix Vite static copy plugin classifiers destination path

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         },
         {
           src: "src/classifiers/**/*",
-          dest: "classifiers",
+          dest: "src/classifiers",
         },
       ],
     }),
